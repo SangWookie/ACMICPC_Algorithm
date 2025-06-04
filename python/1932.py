82c56c1f72a46d4881812057dd72ae6b3d409289
SangWookie <swcube520@gmail.com>
2025-06-04 10:36:00 +0900

    solve: 1932

--- python/1932.py
+++ python/1932.py
@@ -3,4 +3,13 @@ n = int(input())
 triangle = []
 for _ in range(n):
     row = list(map(int, input().split()))
-    
+    triangle.append(row)
+for i in range(1, n):
+    for j in range(len(triangle[i])):
+        if j == 0:
+            triangle[i][j] += triangle[i-1][j]
+        elif j == len(triangle[i]) - 1:
+            triangle[i][j] += triangle[i-1][j-1]
+        else:
+            triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+print(max(triangle[n-1]))
